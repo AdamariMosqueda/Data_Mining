a33311a69ac11ada5df06e1fd176535934319050
AdamariMosqueda <adamari.mosqueda16@tectijuana.edu.mx>
2020-12-15 10:56:59 -0800

    Elbow Method

--- Unit 4/Evaluation/Evaluation 4.R
+++ Unit 4/Evaluation/Evaluation 4.R
@@ -6,3 +6,24 @@
 #Al finalizar el desarrollo explicar detalladamente en consiste el modelo de
 #agrupación K-Means y cuales fuerón sus observacines en el analisis de visualización
 #de datos.
+
+setwd("C:/")
+getwd()
+
+dataset = read.csv('iris.csv')
+dataset = dataset[1:4]
+dataset
+
+
+set.seed(6)
+
+wcss = vector()
+                   #suma de cuadrados dentro de cada grupo
+for (i in 1:10) wcss[i] = sum(kmeans(dataset, i)$withinss)
+
+plot(1:10,
+     wcss,
+     type = 'b',
+     main = paste('The Elbow Method'),
+     xlab = 'Number of clusters',
+     ylab = 'WCSS')
