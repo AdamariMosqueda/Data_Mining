794baf7157d36737c1852ff038b7679cebae7ba7
AdamariMosqueda <adamari.mosqueda16@tectijuana.edu.mx>
2020-12-15 10:37:28 -0800

    Add files for the exam

---
+++ Unit 4/Evaluation/Evaluation 4.R
@@ -0,0 +1,8 @@
+#Implementar el modelo de agrupación K-Means con el conjunto de datos Iris.csv que
+#se encuentra en https://github.com/jcromerohdz/iris utilizando el método
+#kmeans() en R. Una vez que se obtenga el modelo de agrupamiento hacer el analis
+#de visualización de datos correspondiente.
+
+#Al finalizar el desarrollo explicar detalladamente en consiste el modelo de
+#agrupación K-Means y cuales fuerón sus observacines en el analisis de visualización
+#de datos.
